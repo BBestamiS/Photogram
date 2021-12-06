6ea6e661befc4ac9fd546cf2db9970bf3a50369f
BBestamiS <technosor202@outlook.com>
2021-12-06 17:42:55 +0300

    Signup screen design and validation are completed

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:photogram/auth_screens/signup.dart';
+
+Future<void> main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: Signup(),
+    );
+  }
+}
